23de33a7a175086bc35f585b64c08698b61d4347
Hector Figueroa <hfiguero@mex-mp126.santaclara.corp.akamai.com>
2024-03-07 16:14:46 -0600

    def

--- src/def.py
+++ src/def.py
@@ -13,8 +13,15 @@ def fn3(a, b, c):
 def fn4(a=1, b=2):
     return a-b
 
+# https://stackoverflow.com/questions/36901/what-does-double-star-asterisk-and-star-asterisk-do-for-parameters
+# https://docs.python.org/3/tutorial/controlflow.html#more-on-defining-functions
+# https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists
+def foo(name, /, **kwds):
+    return 'name' in kwds
+
 
 print(type(fn(1, 2, 3)))
 print(type(fn2(1, 2, 3)))
 print(type(fn3(1, 2, 3)))
 print(fn4(b=2, a=1))
+print(foo(1, **{'name': 2}))
